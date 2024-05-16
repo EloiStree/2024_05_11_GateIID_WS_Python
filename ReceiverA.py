bb3e376eed037043c4aed34c281b130295cf18fe
Éloi Strée <eloistree@gmail.com>
2024-05-16 22:17:20 +0200

    Save

---
+++ ReceiverA.py
@@ -0,0 +1,7 @@
+# ReceiverA.py
+
+
+
+def notify_received_iid(int_index, int_value, ulong_date):
+    a=0
+    #print("CR|IID received: index=%d, value=%d, date=%d" % (int_index, int_value, ulong_date))
